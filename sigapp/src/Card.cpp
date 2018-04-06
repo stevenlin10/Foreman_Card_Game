86b2258b2ccd645114ab66f19c1536eb727c3813
jforeman7 <jeffforeman6849@gmail.com>
2018-04-05 20:56:38 -0800

    Added Card.cpp and edited Card.h

---
+++ sigapp/src/Card.cpp
@@ -0,0 +1,9 @@
+#include "Card.h"
+
+/*
+	Function definitions of the card class. Please reference Card.h for more abstract details.
+
+	Written by Jeff Foreman, 5 April 2018
+*/
+
+Card::Card(){}
--- sigapp/src/Card.h
+++ sigapp/src/Card.h
@@ -1,12 +1,19 @@
 #pragma once
 
+/*
+	This header file defines the structure of the class Card. 
+	A Card belongs to one of four suits, and holds a value of either two through ten, a face value, or is an ace.
+	These define the individual units of a Deck.
+
+	Written by Jeff Foreman, 5 April 2018
+*/
 class Card
 {
 protected:
-	enum Suit {Clubs, Diamonds, Hearts, Spades};
-	enum Face {Jack, Queen, King, Ace};
+	enum Suit {Clubs, Diamonds, Hearts, Spades}; // Enumerate the card suits.
+	const int Ace = 1, Jack = 11, Queen = 12, King = 13; // Declare the special values of the face cards and the ace.
 
 public:
-
+	Card();
 
 };
